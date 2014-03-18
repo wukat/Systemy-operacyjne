292fcbb134593cbac29e2b0ba07dbdbedd811dd9
Wojciech Kasperek <wojciechksprk@gmail.com>
2014-03-18 16:10:50 +0100

    changes in cat

--- lab2/cat.c
+++ lab2/cat.c
@@ -13,72 +13,51 @@
 #include "readall.h"
 
 #define BUFSIZE 512
-/*
-void cat(int fdFrom) {
-	char buf[BUFSIZE];
-	ssize_t n;
-	while((n = readall(fdFrom, buf, sizeof(buf))) != -1) { 
-		writeall(1, buf, n);
-		if (n == 0)
-			return;
-	}
-}*/
 
-/* second version */
-void cat2(char * fileFrom, char * option) {
-	char buf[BUFSIZE];
-	FILE* f = NULL;
-	char * line = NULL;
+void cat(int fdFrom, char * option) {
 	int number = 0;
-	if (option[0] == '-' && option[1] == 'n')
+	char buf[BUFSIZE];
+	ssize_t nread, nwrite;
+	if (option != NULL && option[0] == '-' && option[1] == 'n')
 		number = 1;
-	if (fileFrom != NULL) {
-		if ((f = fopen(fileFrom, "r")) != NULL) {
-			while((line = fgets(buf, 512, f)) != NULL) {
-				if (number)
-					fprintf(stdout, "%d. ", number++);
-				fputs(line, stdout);
-			}
-		} else 
-			perror("Opening file failed");
+	while ((nread = read(fdFrom, buf, sizeof(buf))) > 0) {
+		if (number)
+			fprintf(stdout, "%d. ", number++);
+   		if ((nwrite = writeall(1, buf, nread)) == -1) {
+    			perror("Błąd w zapisie");
+    			return;
+    		}
 	}
-	else {
-		while((line = fgets(buf, 512, stdin)) != NULL) {
-			if (number)
-				fprintf(stdout, "%d", number++);
-			fputs(line, stdout);
+}
+
+void useCat(char * fileFrom, char * option) {
+	int fromfd = -1;
+	if (fileFrom != NULL) {
+		if ((fromfd = open(fileFrom, O_RDONLY)) != -1) {
+			cat(fromfd, option);
+			close(fromfd);
+		}
+		else {
+			if (errno == ENOENT)
+				printf("Plik nie istnieje");
+			else if (errno == EACCES)
+				printf("Brak dostępu do pliku");
 		}
 	}
-
-}
-/*
-void useCat(char * fileFrom) {
-	int fromfd = -1;
-	if ((fromfd = open(fileFrom, O_RDONLY)) != -1) {
-		cat(fromfd);
-		close(fromfd);
-	}
 	else {
-		if (errno == ENOENT)
-			printf("Pliknie istnieje");
-		else if (errno == EACCES)
-			printf("Brak dostępu do pliku");
+		cat(0, option);
 	}
-}*/
+}
 
 int main(int argc, char **argv) {
 	if (argc == 1) {
-		/*cat(0);*/
-		cat2(NULL, NULL);
+		cat(0, NULL);
 	}
-	else if (argc == 2) {
-		if (argv[1][0] != '-')
-			cat2(argv[1], NULL);
-		else
-			cat2(NULL, argv[1]);
-		/*useCat(argv[1]);*/
-	} else if (argc == 3) {
-		cat2(argv[2], argv[1]);
+	else {
+		int i = 1;
+		for (i = 1; i < argc; i++) {
+			useCat(argv[i], NULL);
+		}
 	}
 	
 	return 0;
