c4bad586dc2fddde79db8c090931d8ea933959cf
wukat <wojciechksprk@gmail.com>
2014-03-18 11:51:45 +0100

    refator

--- lab2/copy1.c
+++ lab2/copy1.c
@@ -33,22 +33,23 @@ void copy(char *from, char *to)  /* has a bug ??*/
 	if ((fromfd = open(from, O_RDONLY)) != -1) {
 		if ((tofd = open(to, O_WRONLY | O_CREAT | O_TRUNC,
 				S_IRUSR | S_IWUSR)) != -1) {
-			while ((nread = read(fromfd, buf, sizeof(buf))) > 0)
+			while ((nread = read(fromfd, buf, sizeof(buf))) > 0) {
 	    		if ((nwrite = writeall(tofd, buf, nread)) == -1) {
 	    			perror("Błąd w zapisie");
 	    			break;
 	    		}
+	    	}
 	    	if (nread == -1) {
 	    		perror("Błąd w czytaniu pliku");
 	    	}
         	close(fromfd);
-    		}
-        	else {
-        		if (errno == ENOENT)
-					printf("Plik drugi nie istnieje");	
-				else if (errno == EACCES)
-					printf("Brak dostępu do drugiego pliku");
-			}
+    	}
+        else {
+        	if (errno == ENOENT)
+				printf("Plik drugi nie istnieje");	
+			else if (errno == EACCES)
+				printf("Brak dostępu do drugiego pliku");
+		}
 		close(tofd);
 	}
 	else {
@@ -61,8 +62,7 @@ void copy(char *from, char *to)  /* has a bug ??*/
 }
  
 int main(int argc, char **argv){
-	if (argc != 3)
-	{
+	if (argc != 3) {
 		fprintf(stderr,"usage: %s from_pathname to_pathname\n", argv[0]);
 		return 1;
 	}
