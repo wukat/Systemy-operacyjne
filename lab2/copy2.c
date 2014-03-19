01b24f499c851d7d8a8438667220e15a7a2fc518
wukat <wojciechksprk@gmail.com>
2014-03-19 23:00:28 +0100

    removed/renamed

---
+++ lab2/copy2.c
@@ -0,0 +1,25 @@
+/*author wukat*/
+/*uses copied functions*/
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "writeall.h"
+#include "special.h" /*includes only definition of buffersize*/
+#include "copy1.h"
+#include "t.h"
+
+
+int main(int argc, char **argv) {
+	if (argc != 3) {
+		fprintf(stderr,"usage: %s from_pathname to_pathname\n", argv[0]);
+		return 1;
+	}
+	timestart();
+	copy(argv[1], argv[2]);
+	timestop("Copy2");
+	return 0;
+}
