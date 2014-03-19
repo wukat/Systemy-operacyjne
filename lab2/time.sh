e93cbd68216764d8217e81402fbb7e15146f2891
wukat <wojciechksprk@gmail.com>
2014-03-19 14:45:03 +0100

    for loop instead of manual writing

--- lab2/time.sh
+++ lab2/time.sh
@@ -3,24 +3,15 @@
 
 dd if=/dev/zero of=bigfile count=10k
 gcc -Wall -ansi -pedantic copy3.c t.c -o copy3
-./copy3 bigfile second.second
-echo "#define BUFSIZE 1" > special.h
-echo "Buffer set size to 1"
-gcc -Wall -ansi -pedantic checkTimes.c t.c -o copy2
-./copy2 bigfile second1.second
-echo "#define BUFSIZE 512" > special.h
-echo "Buffer set size to 512"
-gcc -Wall -ansi -pedantic checkTimes.c t.c -o copy2
-./copy2 bigfile second2.second
-echo "#define BUFSIZE 1024" > special.h
-echo "Buffer size set to 1024"
-gcc -Wall -ansi -pedantic checkTimes.c t.c -o copy2
-./copy2 bigfile second3.second
-echo "#define BUFSIZE 1100" > special.h
-echo "Buffer size set to 1100"
-gcc -Wall -ansi -pedantic checkTimes.c t.c -o copy2
-./copy2 bigfile second4.second
-rm *.second
+./copy3 bigfile second
+for i in 1 512 1024 1100 ; do
+	echo "#define BUFSIZE $i" > special.h
+	echo "Buffer set size to $i"
+	gcc -Wall -ansi -pedantic checkTimes.c t.c -o copy2
+	./copy2 bigfile second
+done
+
+rm second
 rm bigfile
 rm copy3 
 rm copy2
