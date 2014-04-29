a9b4063f236805cdccbf56eb58ad393ec38315eb
Wojciech Kasperek <wojciechksprk@gmail.com>
2014-04-29 17:01:03 +0200

    3

---
+++ lab6/exWithDict.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	int pfd1[2], pfd2[2];
+	pid_t pid1, pid2;
+	iont 
+	
+	if (pipe(pfd1) == -1) {
+		perror("Something went wrong with pipe:");
+		exit(EXIT_FAILURE);
+	}
+	if (pipe(pfd2) == -1) {
+		perror("Something went wrong with pipe:");
+		exit(EXIT_FAILURE);
+	}
+	pid1 = fork();
+	if (pid1 == 0) {
+		while ()
+	} else {
+		pid2 = fork();
+	}
+
+	return 0;
+}
+
