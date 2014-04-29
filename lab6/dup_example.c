ea0607c36cb48e0fff7290935d4a329ac67d9438
Wojciech Kasperek <wojciechksprk@gmail.com>
2014-04-29 16:55:39 +0200

    second program

--- lab6/dup_example.c
+++ lab6/dup_example.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
  
 int main(void)
 {
@@ -8,15 +11,21 @@ int main(void)
         char    string[] = "Test";
         char    buf[10];
  
-        pipe(pfd);
+        if (pipe(pfd) == -1) {
+		perror("Something went wrong with pipe:");
+		exit(EXIT_FAILURE);
+	}
         pid = fork();
  
         if(pid == 0) {
-		close(0);              
- 		dup(pfd[0]);
+		close(STDIN_FILENO);       /* zamykamy 0,  bo dup go pozniej uzyje jako najnizszego */      
+ 		dup2(pfd[0], STDIN_FILENO);
+		close(pfd[1]);
+		close(pfd[0]);
         	read(STDIN_FILENO, buf, sizeof(buf));
                 printf("Wypisuje: %s", buf);
         } else {
+		close(pfd[0]);
         	write(pfd[1], string, (strlen(string)+1));        
 	}
  
