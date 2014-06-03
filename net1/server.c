964f45acc18fee5db77f09565f98b9323f38374a
Wojciech Kasperek <wojciechksprk@gmail.com>
2014-06-03 16:58:29 +0200

    network lab

---
+++ net1/server.c
@@ -0,0 +1,116 @@
+/*
+** server.c -- a stream socket server demo
+*/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+ 
+#define MYPORT 58394	// the port users will be connecting to
+ 
+#define BACKLOG 10	 // how many pending connections queue will hold
+ 
+void sigchld_handler(int s)
+{
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+ 
+int main(void)
+{
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct sockaddr_in my_addr;	// my address information
+	struct sockaddr_in their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1, pid, numbytes;
+	char msg[11];
+ 
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket");
+		exit(1);
+	}
+ 
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		perror("setsockopt");
+		exit(1);
+	}
+ 
+	my_addr.sin_family = AF_INET;		 // host byte order
+	my_addr.sin_port = htons(MYPORT);	 // short, network byte order
+	my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
+	memset(my_addr.sin_zero, '\0', sizeof my_addr.sin_zero);
+ 
+	if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof my_addr) == -1) {
+		perror("bind");
+		exit(1);
+	}
+ 
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+ 
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+ 
+	sin_size = sizeof their_addr;
+	/*while ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) != -1) {
+		if ((pid = fork()) == -1) {
+			perror("fork");
+			break;
+		}
+		if (pid != 0)
+			continue;
+ 		if (new_fd == -1) {
+			perror("accept");
+			break;
+		}
+	
+		printf("server: got connection from %s\n",inet_ntoa(their_addr.sin_addr));
+		if (send(new_fd, "Hello, Wiktor!\n", 14, 0) == -1) {
+			perror("send");
+			break;
+		}
+		sleep(1); // just for observing easily that the server cannot serve a few clients concurrently
+		close(new_fd);
+		break;
+	}*/
+
+/* komunikator */
+	if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) != -1) {
+		if ((pid = fork()) == -1) {
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0) {	
+			while (msg[0] != EOF) {
+				scanf("%s", msg);
+				if (send(new_fd, msg, strlen(msg), 0) == -1) {
+					perror("send");
+					exit(EXIT_FAILURE);
+				}
+			}
+			exit(EXIT_SUCCESS);
+		}
+		while ((numbytes=recv(new_fd, msg, 10, 0)) != -1) {
+	    		printf("Received: %s", msg);
+		}
+		perror("oo");
+		exit(EXIT_FAILURE);	 
+	}
+ 
+	return 0;
+}
